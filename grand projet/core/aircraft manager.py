8dae4ef9a7b755b9f75163753256ed2dcdc983fa
luka <dulkan.luka.13@gmail.com>
2025-12-09 00:36:37 +0100

    1

---
+++ grand projet/core/aircraft manager.py
@@ -0,0 +1,67 @@
+import random
+from math import sqrt
+from core.aircraft import Aircraft
+
+class AircraftManager:
+
+    COLLISION_HORITZ_DIST = 4000
+    COLLISION_VERT_DIST = 1000
+    SPAWN_PROBABILITY = 0.01
+
+    def __init__(self):
+        self.aircraft_list = []
+
+    def add_aircraft(self, aircraft: Aircraft):
+        self.aircraft_list.append(aircraft)
+
+    def remove_aircraft(selfself, aircraft: Aircraft):
+        if aircraft in self.aircraft_list:
+            self.aircraft_list.remove(aircraft)
+
+    def generate_random_aircraft(self):
+        airlines = ["AF", "BA", "LH", "DL", "UA", "RYR", "EZY"]
+        code = random.choice(airlines) + str(random.randint(100, 999))
+        new_ac = Aircraft(code)
+        self.aircraft_list.append(new_ac)
+
+    def detect_collision(self):
+        for ac in self.aircraft_list:
+            ac.in-collision = False
+        n = len(self.aircraft_list)
+        horiz_sq = self.COLLISION_HORIZ_DIST ** 2
+
+        for i in range(n):
+            a1 = self.aircraft_list[i]
+            if a1.status == "Atteri":
+                continue
+
+            for j in range(i+1, n):
+                a2 = self.aircraft_list[j]
+                if a2.status == "Atteri":
+                    continue
+
+                dx = a1.x - a2.x
+                dy = a1.y - a2.y
+                dist_sq = dx * dx + dy * dy
+
+                d_alt = abs(a1.altitude - a2.altitude)
+
+                if dist_sq < horiz_sq and d_alt < self.COLLISION_VERT_DIST:
+                    a1.in_collision = True
+                    a2.in_collision = True
+
+    def update_movement(self, dt: float):
+        for ac in self.aircraft_list:
+            ac.movement(dt)
+
+    def update(self, dt = 0.1):
+        self.detect_collision()
+        self.update_movement(dt)
+
+        if random.random() < self.SPAWN_PROBABILITY:
+            return self.generate_random_aircraft()
+
+        return None
+    def get_all_aircrafts(self):
+        return self.aircraft_list
+
--- grand projet/gui/main_window.py
+++ grand projet/gui/main_window.py
@@ -1,1 +1,118 @@
-print('test')
+from PySide6.QtWidgets import (QMainWindow, QWidget, QLabel, QPushButton, QHBoxLayout, QVBoxLayout, QStatusBar,
+                               QLineEdit, QApplication, QMessageBox, QFrame, QGridLayout, QFormLayout)
+from PySide6.QtCore import Qt, QTimer
+from core.aircraft_manager import AircraftManager
+from gui.radar_view import RadarScene, RadarView
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Contrôle Aérien")
+        self.setGeometry(100, 100, 1400, 800)
+
+        self.manager = AircraftManager()
+        self.current_aircraft_symbol = None
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        main_layout = QHBoxLayout(central_widget)
+
+        self.radar_scene = RadarScene()
+        self.radar_view = RadarView(self.radar_scene)
+        main_layout.addWidget(self.radar_view)
+
+        control_panel = self.build_right_panel()
+        main_layout.addWidget(control_panel)
+
+        self.statusbar = QStatusBar()
+        self.setStatusBar(self.statusbar)
+
+        self.radar_scene.selectionChanged.connect(self, on_aircraft_selected)
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_simulation)
+        self.timer.start(100)
+
+    def build_right_panel(self):
+        panel = QFrame()
+        panel.setFixedWidht(260)
+        layout = QVBoxLayout(panel)
+
+        self.info_label = QLabel()
+        self.info_label.setStyleSheet("font-weight; bold; font-size: 14px;")
+        layout.addWidget(self.info_label)
+
+        self.info_label = QLabel("Sélectionnez un avion.")
+        self.info_label.setAlignment(Qt.AlignTop)
+        self.info_label.setMinimumHeight(200)
+        layout.addWidget(self.info_label)
+
+        form_group = QFrame()
+        form_group.setStyleSheet("border: 1px solid grey; padding: 5px; border-radius: 5px;")
+        form_layout = QGridLayout(form_group)
+
+        form_layout.addWidget(QLabel("Alt. (ft):"), 0, 0)
+        self.input_alt = QLineEdit()
+        form_layout.addWidget(self.input_alt, 0, 1)
+
+        form_layout.addWidget(QLabel("Vitesse (kts):"), 1, 0)
+        self.input_vites = QLineEdit()
+        form_layout.addWidget(self.input_vites, 1, 1)
+
+
+        form_layout.addWidget(QLabel("Cap (deg):"), 2, 0)
+        self.input_cap = QLineEdit()
+        form_layout.addWidget(self.input_cap, 2, 1)
+
+        slef.btn_apply = QPushButton("Envoyer Instructions")
+        self.btn_apply.clicked.connect(self.send_instruction)
+        slef.btn_apply.setEnable(False)
+        form_layout.addWidget(slef.btn_apply, 3, 0, 1, 2)
+
+        layout.addWidget(form_group)
+        layout.addStretch()
+
+        return panel
+
+
+    def on_aircraft_selected(self):
+        items = self.radar_scene.selectedItems()
+        slef.current_aircraft_symbol = None
+
+        if not items :
+            self.btn_apply.setEnabled(False)
+            self.input_alt.clear()
+            self.input_speed.clear()
+            self.input_heading.clear()
+            return
+
+        symbol = items[0]
+
+        if hasattr(symbol, "aircraft"):
+            self.current_aircraft_symbol = symbol
+            data = symbol.aircraft.get_data()
+
+            self.input_alt.setText(str(data['Target Alt (ft)']))
+            self.input_speed.setText(str(data['Target Speed (kts)']))
+            self.input_heading.setText(str(data['Target Cap (deg)']))
+
+            self.btn_apply.setEnabled(True)
+            self.update_info_display()
+
+    def update_info_display(self):
+        if not self.current_aircraft_symbol:
+            return
+        data = ("red" if data["Collision"] else ("blue" if data["Status"] == "Atteri" else "green"))
+        h text = (
+            f"Avion : {data['ID']}\n"
+            f"Statut : {status}\n"
+            f"Altitude : {data['Altitude (ft)']} → {data['Target Alt (ft)']}\n"
+            f"Vitesse : {data['Vitesse (kts)']} → {data['Target Speed (kts)']}\n"
+            f"Cap : {data['Cap (deg)']}° → {data['Target Cap (deg)']}°\n"
+            f"Carburant : {data['Carburant (min)']:.1f} min"
+        )
+         self.info_label.setText(text)
+
+
+

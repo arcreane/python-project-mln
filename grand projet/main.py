5e96f7b8fc356bc2300665fb1c53867c848e0c32
melvine <melvine.le-bris@ipsa.fr>
2025-12-09 18:44:12 +0100

    Check connection

--- grand projet/main.py
+++ grand projet/main.py
@@ -1,10 +1,25 @@
 import sys
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel
+from PySide6.QtGui import QIcon, QPixmap
 
-from PySide6.QtWidgets import QApplication, QWidget
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Simulation Tour de controle")
+        self.setFixedSize(1280, 720)
+
+        self.button1 = QPushButton("button 1")
+        self.button1.clicked.connect(self.boutton_clique)
+
+        self.setCentralWidget(self.button1)
+
+    def boutton_clique(self):
+        print("clique")
+
 
 app = QApplication(sys.argv)
 
-window = QWidget()
+window = MainWindow()
 window.show()
 
 app.exec()
